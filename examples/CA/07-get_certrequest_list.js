db78fd1e470c5cd719703be82afcbdbad66216ad
Maxim Kanashin <kam@digt.ru>
2019-09-16 18:00:31 +0300

    auto fix prettier problems

--- examples/CA/07-get_certrequest_list.js
+++ examples/CA/07-get_certrequest_list.js
@@ -1,22 +1,23 @@
 const { Curl } = require('../../dist')
 
 const curl = new Curl()
-const url =
-'https://testca2012.cryptopro.ru/ui/api/certrequest'
+const url = 'https://testca2012.cryptopro.ru/ui/api/certrequest'
 var name = 'g12'
 var password = '9185543195'
 
 curl.setOpt(Curl.option.URL, url)
 //curl.setOpt('SSL_VERIFYHOST', 0)
 //curl.setOpt('SSL_VERIFYPEER', 0)
-curl.setOpt(Curl.option.HTTPHEADER,
-    ['Authorization: Basic ' + Buffer.from(name + ':' + password).toString('base64')]);
+curl.setOpt(Curl.option.HTTPHEADER, [
+  'Authorization: Basic ' +
+    Buffer.from(name + ':' + password).toString('base64'),
+])
 
 curl.on('end', (statusCode, body) => {
-    console.log(body)
-    curl.close()
-  })
-  
-  curl.on('error', curl.close.bind(curl))
-  
-  curl.perform()
+  console.log(body)
+  curl.close()
+})
+
+curl.on('error', curl.close.bind(curl))
+
+curl.perform()
