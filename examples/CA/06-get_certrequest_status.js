c1519f32a095c9eb5a7ca571db52c9e496c3194a
Maxim Kanashin <kam@digt.ru>
2019-09-16 16:32:59 +0300

    Update 06-get_certrequest_status.js
    https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/161

--- examples/CA/06-get_certrequest_status.js
+++ examples/CA/06-get_certrequest_status.js
@@ -3,6 +3,7 @@ const { Curl } = require('../../dist')
 const curl = new Curl()
 const url =
 'https://testca2012.cryptopro.ru/ui/api/certrequest/dda305a3-5942-420a-b317-aaca00dde8df' //{GUID}
+//https://testca2012.cryptopro.ru/ui/api/certrequest/dda305a3-5942-420a-b317-aaca00dde8df/rawcert -- for binary output
 var name = 'g12'
 var password = '9185543195'
 
