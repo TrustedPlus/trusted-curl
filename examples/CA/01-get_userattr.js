a7fdd9094ff37b0752b0e13443501f4c9600058c
Aleksandr Gavrilov <alg@digt.ru>
2019-09-16 15:24:21 +0300

    auto fix prettier problems

--- examples/CA/01-get_userattr.js
+++ examples/CA/01-get_userattr.js
@@ -1,7 +1,8 @@
 const { Curl } = require('../../dist')
 
 const curl = new Curl()
-const url = 'https://testca2012.cryptopro.ru/ui/api/b1ca4992-d7cd-4f7e-b56e-a81e00db58ee/userattr'
+const url =
+  'https://testca2012.cryptopro.ru/ui/api/b1ca4992-d7cd-4f7e-b56e-a81e00db58ee/userattr'
 
 curl.setOpt(Curl.option.URL, url)
 //curl.setOpt('SSL_VERIFYHOST', 0)
